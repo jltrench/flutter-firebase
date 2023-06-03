636345f5edb9cb112e7c369defeb0fac2b8ad172
João Lucca Trench <jltrench@gmail.com>
2023-06-03 20:45:50 -0300

    Projeto inicial, Firebase configurado

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  firestore
+      .collection('Testando')
+      .doc('Ainda testando')
+      .set({"funcionou?": true});
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: const Text('A'),
+      ),
+    );
+  }
+}
